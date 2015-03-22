933e062236c4ad22ff68367628032c5126b68ef0
B130296 <b130296@sagitario.cic.ipn.mx>
2015-03-21 22:18:32 -0600

    Comments finished

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+#############################################################################
+#                                 Coursera                                  #
+#                                                                           #
+#                          Johns Hopkins University                         #
+#                            R Programming Course                           #
+#                             Roger D. Peng, PhD                            #
+#                                 March, 2015                               #
+#############################################################################
 
-## Write a short comment describing this function
+#        ==============    Programming Assignment 2   ==============        #
+
+# The second programming assignment requires to write an R function able to #
+# cache the inverse of a matrix to avoid compute it repeate repeatedly.     #
+#                                                                           #
+# This solution is based on the example shown in the assignment example.    #
+# After implementatiom the concept and fuction was tested as follows:       #
+#                                                                           #
+# - A 1000x1000 matrix is defined.                                          #
+# - It is randomly filled with 0s an 1s.                                    #
+# - Time to get the inverse is checked.                                     #
+# - Matrix is cached and its inverse is calculated using the function which #
+#   checks if the inverse was calculated.                                   #
+# - Time to get the inverse is checked.                                     #
+#                                                                           #
+# > m<-matrix(0, ncol=1000, nrow=1000)                                      #
+# > m<-apply(m, c(1,2), function(x) sample(c(0,1),1))                       #
+# > system.time(solve(m))                                                   #
+# user  system elapsed                                                      #
+# 1.884   0.005   1.982                                                     #
+# > system.time(solve(m))                                                   #
+# user  system elapsed                                                      #
+# 1.918   0.004   1.923                                                     #
+# > x <- makeCacheMatrix(m)                                                 #
+# > system.time(cacheSolve(x))                                              #
+# user  system elapsed                                                      #
+# 1.868   0.004   1.872                                                     #
+# > system.time(cacheSolve(x))                                              #
+# getting cached data                                                       #
+# user  system elapsed                                                      #
+# 0       0       0                                                         #
+# ========================================================================= #
+
+# makeCacheMatrix.- This function creates a special "matrix" object that    #
+# can cache its inverse. It receives a matrix and create a list of          #
+# functions that "get" and "set" the value of the matrix and its inverse    #
+# (using the "solve()" buit-in function of R).                              #
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -18,7 +60,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+# cacheSolve.- This function calculates the inverse of a matrix. It first    #
+# checks to see if it has already been calculated. If so, it gets the        #
+# inverse from the cache and skips the computation. Otherwise, it calls the  #
+# solve() function sets the value in the cache via the setinvm function      #
+# defind in makeCacheMatrix().                                               #
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -32,3 +78,5 @@ cacheSolve <- function(x, ...) {
         x$setinvm(m)
         m
 }
+
+#############################################################################
